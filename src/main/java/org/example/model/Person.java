a081fc3101f10dc34644e6d773cd36d3452b452f
Daniel Wennström <daniel@wennstrom.io>
2025-02-04 13:32:44 +0100

    Person class now has an AppUser field

--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -1,18 +1,22 @@
 package org.example.model;
 
+import java.util.Objects;
+
 public class Person {
     private int id;
     private String firstName;
     private String lastName;
     private String email;
+    private AppUser credentials;
 
-    public Person(int id, String firstName, String lastName, String email) {
+    public Person(int id, String firstName, String lastName, String email, AppUser credentials) {
         this.id = id;
-        if (firstName == null || lastName == null || email == null)
-            throw new IllegalArgumentException("First name, last name, and email cannot be null");
+        if (firstName == null || lastName == null || email == null || credentials == null)
+            throw new IllegalArgumentException("First name, last name, email, and credentials can't be null.");
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.credentials = credentials;
     }
 
     public int getId() {
@@ -29,7 +33,7 @@ public class Person {
 
     public void setFirstName(String firstName) {
         if (firstName == null || firstName.isEmpty()) {
-            throw new IllegalArgumentException("First name cannot be null or empty");
+            throw new IllegalArgumentException("First name can't be null or empty");
         }
         this.firstName = firstName;
     }
@@ -40,7 +44,7 @@ public class Person {
 
     public void setLastName(String lastName) {
         if (lastName == null || lastName.isEmpty()) {
-            throw new IllegalArgumentException("Last name cannot be null or empty");
+            throw new IllegalArgumentException("Last name can't be null or empty");
         }
         this.lastName = lastName;
     }
@@ -51,12 +55,41 @@ public class Person {
 
     public void setEmail(String email) {
         if (email == null || !email.matches("\\S+@\\S+\\.\\S+")) {
-            throw new IllegalArgumentException("Invalid email address");
+            throw new IllegalArgumentException("Invalid email format or null.");
         }
         this.email = email;
     }
 
-    public String getSummary() {
-        return "Person id: " + getId() + ", firstName: '" + getFirstName() + "', lastName: '" + getLastName() + "', email: " + getEmail();
+    public AppUser getCredentials() {
+        return credentials;
+    }
+
+    public void setCredentials(AppUser credentials) {
+        if (credentials == null) {
+            throw new IllegalArgumentException("Credentials can't be null.");
+        }
+        this.credentials = credentials;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "email='" + email + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return getId() == person.getId() && Objects.equals(getFirstName(), person.getFirstName()) && Objects.equals(getLastName(), person.getLastName()) && Objects.equals(getEmail(), person.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getFirstName(), getLastName(), getEmail());
     }
 }
