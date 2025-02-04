316bfb76b03c884a8ee069aa6c712fb786cbe1e5
Daniel Wennström <daniel@wennstrom.io>
2025-02-04 13:42:34 +0100

    AppUser test and changed Person references to take credentials

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -2,9 +2,11 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.example.model.AppUser;
 import org.example.model.Person;
 import org.example.model.TodoItem;
 import org.example.model.TodoItemTask;
+import org.example.util.AppRole;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,21 +15,37 @@ import java.time.LocalDate;
 
 class MainTest {
     private Person person;
+    private AppUser credentials;
     private LocalDate today;
 
     @BeforeEach
     public void setUp() {
-         person = new Person(1, "Sven", "Svensson", "test@domain.com");
-         today = LocalDate.now();
+        credentials = new AppUser("sven_svensson", "1234", AppRole.ROLE_APP_USER);
+        person = new Person(1, "Sven", "Svensson", "test@domain.com", credentials);
+        today = LocalDate.now();
     }
 
     @Test
     @DisplayName("Test Person")
     void testPerson() {
-        Person person = new Person(1, "Sven", "Svensson", "test@domain.com");
+        Person person = new Person(1, "Sven", "Svensson", "test@domain.com", credentials);
 
         assertEquals("Sven Svensson", String.format("%s %s", person.getFirstName(), person.getLastName()));
         assertEquals("test@domain.com", person.getEmail());
+    }
+
+    @Test
+    @DisplayName("Test AppUser")
+    void testAppUser() {
+        AppRole role = AppRole.ROLE_APP_USER;
+        String username = "username";
+        String password = "password";
+
+        AppUser user = new AppUser(username, password, role);
+
+        assertEquals(username, user.getUsername());
+        assertEquals(password, user.getPassword());
+        assertEquals(role, user.getRole());
     }
 
     @Test
